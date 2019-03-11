9aa1376b8171949c754bca640639ab02549e1313
slimwizard <mrice@Matthews-MacBook-Pro-2.local>
2019-03-10 19:35:26 -0500

    added more cards and styling/'typed out' header effect on About page

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -9,11 +9,25 @@ export class AboutMeComponent implements OnInit {
 
   constructor() { }
 
+  helloText = ['h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '!']
+  index = 0
+  hello = ''
 
-
-  ngOnInit() {
+  sayHello() : any {
+    let text = document.getElementById('hi-text-2')
+    if (this.index > this.helloText.length-1) {
+      let wave = document.getElementById('wave')
+      wave.style.opacity = '1' ;
+      return
+    };
+    this.hello += this.helloText[this.index]
+    this.index = this.index +1
+    setTimeout(()=>{this.sayHello()}, 100)
   }
 
-
-
+  ngOnInit() {
+    setTimeout(() => {
+      this.sayHello()
+    }, 13);
+  }
 }
